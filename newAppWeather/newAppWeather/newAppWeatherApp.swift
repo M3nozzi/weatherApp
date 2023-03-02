329b13e93f5126c377ffdf1550c9f9c7183f308b
Fabio Menozzi <f.menozzi@yahoo.com>
2023-03-02 19:06:49 -0300

    Creating the app configuration to start the project

---
+++ newAppWeather/newAppWeather/newAppWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppWeatherApp.swift
+//  newAppWeather
+//
+//  Created by Fabio Costa Menozzi on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct newAppWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
